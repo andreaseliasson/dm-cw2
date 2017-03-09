581833bfbdc0c61b67bb17681a78f448fffa6e71
Andreas Eliason <andreashg.eliasson@gmail.com>
2017-03-09 12:45:30 +0000

    Add functions for getting the top k most significant terms in both all docs and in a single doc

--- main.py
+++ main.py
@@ -10,36 +10,43 @@ if __name__ == "__main__":
         text_docs = get_html_files_from_sub_dirs('../data/gap-html', sub_dirs)
         raw_docs = parse_html_files(text_docs)
         to_csv(raw_docs)
-    else:
-        raw_docs_df = pd.read_csv('../data/raw_text.csv', usecols=['id', 'raw_text'])
-        raw_text_list = raw_docs_df['raw_text'].values.tolist()
-        print(raw_text_list[0][:10])
-        print(len(raw_text_list[0]))
-        print(len(raw_text_list))
 
-        tokenized_docs = []
-        stemmed_docs = []
+    raw_docs_df = pd.read_csv('../data/raw_text.csv', usecols=['id', 'raw_text'])
+    raw_text_list = raw_docs_df['raw_text'].values.tolist()
+    print(raw_text_list[0][:10])
+    print(len(raw_text_list[0]))
+    print(len(raw_text_list))
 
-        for doc in raw_text_list[:2]:  # Restrict to just the first two docs for testing purposes
-            tokenized_doc = tokenize_remove_punct(doc)
-            filter_tokenized_doc = filter_words(tokenized_doc)
-            filter_tokenized_doc_non_short = remove_short_words(filter_tokenized_doc)
-            tokenized_docs.append(filter_tokenized_doc_non_short)
+    tokenized_docs = []
+    stemmed_docs = []
 
-            # Stemming
-            stemmed_doc = stem_porter(filter_tokenized_doc_non_short)
-            stemmed_docs.append(stemmed_doc)
-        print(stemmed_docs)
+    for doc in raw_text_list[:3]:  # Restrict to just the first two docs for testing purposes
+        tokenized_doc = tokenize_remove_punct(doc)
+        filter_tokenized_doc = filter_words(tokenized_doc)
+        filter_tokenized_doc_non_short = remove_short_words(filter_tokenized_doc)
+        tokenized_docs.append(filter_tokenized_doc_non_short)
 
-        testing_list_of_strings = ['This is a string', 'This is another string string string yes', 'another doc']
+        # Stemming
+        stemmed_doc = stem_porter(filter_tokenized_doc_non_short)
+        stemmed_docs.append(stemmed_doc)
+    print(stemmed_docs)
 
-        docs_as_strings = tokens_to_string(stemmed_docs)
+    testing_list_of_strings = ['This is a string', 'This is another string string string yes', 'another doc']
 
-        # Bag of words weights matrix
-        bag_of_words = create_bag_of_words(docs_as_strings)
+    docs_as_strings = tokens_to_string(stemmed_docs)
 
-        # Tf-Idf weights matrix
-        tf_idf_weights = create_tf_idf(testing_list_of_strings)
+    # Bag of words weights matrix
+    bag_of_words = create_bag_of_words(docs_as_strings)
 
-        # Cosine similarities
-        cos_similarity = pair_wise_cosine_similarity(tf_idf_weights)
+    # Tf-Idf weights matrix
+    tf_idf_weights, vectorizer = create_tf_idf(testing_list_of_strings)
+
+    # Cosine similarities
+    cos_similarity = pair_wise_cosine_similarity(tf_idf_weights)
+
+    # Get top k significant terms for all documents
+    k_most_significant_terms_for_docs = get_k_most_sign_features_for_docs(vectorizer, 3)
+
+    # Get top k significant terms for a single doc
+    k_most_sign_term_for_doc = get_k_most_significant_terms_for_doc(tf_idf_weights[0], 3, vectorizer)
+
--- tf_idf.py
+++ tf_idf.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 
 def create_tf_idf(list_of_strings):
@@ -16,7 +17,7 @@ def create_tf_idf(list_of_strings):
     print(vocab)
     print(feature_matrix[0])
     print(feature_matrix[1])
-    return feature_matrix
+    return feature_matrix, vectorizer
 
 
 def pair_wise_cosine_similarity(docs_matrix):
@@ -26,3 +27,21 @@ def pair_wise_cosine_similarity(docs_matrix):
     print('cosine similarities')
     print(cos_similarity)
     return cos_similarity
+
+
+def get_k_most_sign_features_for_docs(vectorizer, k):
+    indices = np.argsort(vectorizer.idf_)[::-1]
+    features = vectorizer.get_feature_names()
+    top_features = [features[i] for i in indices[:k]]
+    print('k most significant terms')
+    print(top_features)
+    return top_features
+
+
+def get_k_most_significant_terms_for_doc(weights_vector, k, vectorizer):
+    print('k top doc terms')
+    indices = weights_vector.argsort()[::-1]
+    features = vectorizer.get_feature_names()
+    top_features = [features[i] for i in indices[:k]]
+    print(top_features)
+    return top_features
